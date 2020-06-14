85fee591b78dcf697d241169e708922f4675e3a9
NKWOO <nkw9986@gmail.com>
2020-06-14 13:59:35 +0900

    Test Code Example

---
+++ src/test/java/net/nkwoo/start/web/controller/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package net.nkwoo.start.web.controller;
+
+//강제 Import
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+@DisplayName("컨트롤러 테스트")
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("hello 리턴")
+    public void helloCheck() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
