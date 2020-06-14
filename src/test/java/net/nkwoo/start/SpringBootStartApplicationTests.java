e1de099ef9cf0560c2d43780b28378313f4b4526
NKWOO <nkw9986@gmail.com>
2020-06-14 13:02:25 +0900

    first commit

---
+++ src/test/java/net/nkwoo/start/SpringBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nkwoo.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
