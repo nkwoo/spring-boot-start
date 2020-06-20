6fef23173d7b50697112b497151daeacf49eae97
NKWOO <nkw9986@gmail.com>
2020-06-20 15:00:46 +0900

    Mustache를 이용한 화면 구성 4 게시글 삭제 기능 추가
    index.js 텍스트 오류 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -9,6 +9,10 @@ let index = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
+        
+        $("#btn-delete").on('click', function () {
+            _this.delete();
+        });
     },
     save : function () {
         let data = {
@@ -45,11 +49,26 @@ let index = {
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function () {
-            alert('글이 등록되었습니다.');
+            alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         })
+    },
+    delete : function () {
+        let id = $('#id').val();
+        
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
 }
 
