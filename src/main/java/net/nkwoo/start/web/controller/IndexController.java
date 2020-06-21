8d41e2cf6f9785fd0d9f3595c4756dceff5644da
NKWOO <nkw9986@gmail.com>
2020-06-22 02:41:51 +0900

    구글 로그인 연동 2
    OAuth 인증 방식 소스코드 추가
    현재 미작동 고쳐야함

--- src/main/java/net/nkwoo/start/web/controller/IndexController.java
+++ src/main/java/net/nkwoo/start/web/controller/IndexController.java
@@ -1,6 +1,7 @@
 package net.nkwoo.start.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.nkwoo.start.config.auth.dto.SessionUser;
 import net.nkwoo.start.service.posts.PostsService;
 import net.nkwoo.start.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
 
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
